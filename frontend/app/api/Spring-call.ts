8567b1afd0e1dbdffb04679e1f57143c17a91428
Krish Soni <krish1509soni@gmail.com>
2025-06-22 14:52:52 +0530

    ok

--- frontend/app/api/Spring-call.ts
+++ frontend/app/api/Spring-call.ts
@@ -2,6 +2,6 @@ import axios from "axios";
 
 export const getEvents = async () => {
     const baseUrl = process.env.NEXT_PUBLIC_API_URL;
-    const res = await axios.get(`https://bookmyevent-25dv.onrender.comapi/events`);
+    const res = await axios.get(`https://bookmyevent-25dv.onrender.com/api/events`);
     return res.data;
 };
